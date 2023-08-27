6dc5e4932a41d3ca23cb1b20bd39fc97dc156316
Ahmed Magdy <76597137+Ahmed-Magdyyy@users.noreply.github.com>
2023-08-27 14:13:16 +0300

    first commit

--- utils/sendEmails.js
+++ utils/sendEmails.js
@@ -3,38 +3,44 @@ const nodemailer = require("nodemailer");
 const sendEmail = async (options) => {
   // creating the transporter ( service that will send email like gmail)
   const transporter = nodemailer.createTransport({
-    service: "goDaddy",
-    name: "rafflesksa.com",
-    host: "rafflesksa.com",
+    // service: "goDaddy",
+    name: "smtpout.secureserver.net",
+    host: "smtpout.secureserver.net",
     port: 465, // if secure true => port = 465 || if secure false => port = 587
     secure: true,
 
-  
     // secureConnection: false,
     auth: {
-      user: 'info@rafflesksa.com',
-      pass: 'Raffles@2023',
+      user: "info@rafflesksa.com",
+      pass: "Raffles@2023",
     },
-    tls: { rejectUnauthorized: false }
+    tls: { rejectUnauthorized: false },
   });
 
   // define email options ( from , to , subject , email content )
   const emailOptions = {
     from: "Raffles <info@rafflesksa.com>",
-    to: options.email,
-    subject: options.subject,
-    Text: options.message,
+    to: "bohy.ahmed@gmail.com",
+    subject: `test`,
+    text: "testing email address",
   };
 
   transporter.verify(function (error, success) {
     if (error) {
       console.log(error);
     } else {
+      console.log(success);
       console.log("Server is ready to take our messages");
     }
   });
 
-  await transporter.sendMail(emailOptions)
+  transporter.sendMail(emailOptions, (error, info) => {
+    if (error) {
+      console.log("Error:", error);
+    } else {
+      console.log("Email sent:", info.response);
+    }
+  });
 };
 
 module.exports = sendEmail;
