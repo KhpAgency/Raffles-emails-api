440115d93c00f3cdd00635a1b93e4598fa89a6be
Ahmed Magdy <76597137+Ahmed-Magdyyy@users.noreply.github.com>
2023-08-28 11:00:24 +0300

    first commit

--- utils/sendEmails.js
+++ utils/sendEmails.js
@@ -4,8 +4,8 @@ const sendEmail = async (options) => {
   // creating the transporter ( service that will send email like gmail)
   const transporter = nodemailer.createTransport({
     // service: "goDaddy",
-    name: "mail.rafflesksa.com",
-    host: "mail.rafflesksa.com",
+    name: "rafflesksa.com",
+    host: "rafflesksa.com",
     port: 465, // if secure true => port = 465 || if secure false => port = 587
     secure: true,
 
